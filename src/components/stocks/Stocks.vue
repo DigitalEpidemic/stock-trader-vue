b0078a6af5cc5a54affb01b8856f9fc0144b354b
Jeffrey Polasz <jeff_polasz@hotmail.com>
2020-08-05 23:22:52 -0400

    Navbar + placeholder setup for stocks route

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <app-stock v-for="stock in stocks" :key="stock" />
+  </div>
+</template>
+
+<script>
+import Stock from "./Stock.vue";
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "AMD", price: 85 },
+        { id: 2, name: "AAPL", price: 200 },
+        { id: 3, name: "FB", price: 250 },
+        { id: 4, name: "MSFT", price: 150 },
+      ],
+    };
+  },
+  components: {
+    appStock: Stock,
+  },
+};
+</script>
